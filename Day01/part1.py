0fb4f847d1f152c68b16363ecb9b11738570cc43
Joshua Shroades <167574622+JoshuaShroades@users.noreply.github.com>
2024-12-01 18:09:59 -0500

    Day 1

--- Day01/part1.py
+++ Day01/part1.py
@@ -4,7 +4,19 @@ fileDirectory = os.path.dirname(os.path.realpath(__file__))
 inputFilePath = os.path.join(fileDirectory, "input.txt")
 
 with open(inputFilePath) as inputFile:
-	line = inputFile.readline()
+	lines = [line[:-1] for line in inputFile]
 
 def main():
-	return
+	listA = []
+	listB = []
+	for line in lines:
+		listPair = line.split("   ")
+		listA.append(int(listPair[0]))
+		listB.append(int(listPair[1]))
+	listA.sort()
+	listB.sort()
+	zipList = zip(listA,listB)
+	totalDistance = 0
+	for pair in zipList:
+		totalDistance += abs(pair[0]-pair[1])
+	return totalDistance
--- README.md
+++ README.md
@@ -2,5 +2,5 @@ My solutions for [Advent of Code 2024](https://adventofcode.com/2024), written i
 
 ### Runtimes:  
 Day 1:  
-&nbsp;&nbsp;Part 1: -.-- ms  
-&nbsp;&nbsp;Part 2: -.-- ms  
+&nbsp;&nbsp;Part 1: 0.97 ms  
+&nbsp;&nbsp;Part 2: 11.15 ms  
